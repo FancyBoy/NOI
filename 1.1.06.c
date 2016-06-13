926c7175a0678d243a115a0dd31dd42ada0950b8
FancyBoy <qingming49@163.com>
2016-06-13 18:04:23 +0800

    v2.Accepted
    06: 空格分隔输出

--- 1.1.06.c
+++ 1.1.06.c
@@ -5,6 +5,10 @@ char input_1;
 int input_2;
 float input_3;
 double input_4;
-scanf("%s %d %f %lf",&input_1,&input_2,&input_3,%input_4);
-printf("%s %d %.6f %.6lf",input_1,input_2,input_3,input_4);
+//单个字符要用%c
+scanf("%c",&input_1);
+scanf("%d",&input_2);
+scanf("%f",&input_3);
+scanf("%lf",&input_4);
+printf("%c %d %.6f %.6lf",input_1,input_2,input_3,input_4);
 }
