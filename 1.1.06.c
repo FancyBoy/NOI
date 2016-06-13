aad7de9bf0e79802a002ab0ed2ba65e743b72c2b
FancyBoy <qingming49@163.com>
2016-06-13 18:11:05 +0800

    v3 Accepted (another version)
    06: 空格分隔输出 
    添加多解。

--- 1.1.06.c
+++ 1.1.06.c
@@ -6,9 +6,13 @@ int input_2;
 float input_3;
 double input_4;
 //单个字符要用%c
+scanf("%c %d %f %lf",&input_1,&input_2,&input_3,&input_4);
+/*
+//这个也可以
 scanf("%c",&input_1);
 scanf("%d",&input_2);
 scanf("%f",&input_3);
 scanf("%lf",&input_4);
+*/
 printf("%c %d %.6f %.6lf",input_1,input_2,input_3,input_4);
 }
