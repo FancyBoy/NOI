7fe8407410194fe5155eed313354eda7f66b95ba
FancyBoy <qingming49@163.com>
2016-06-13 17:54:03 +0800

    v1 Compile Error
    06: 空格分隔输出
     状态: Compile Error	
    编译错误信息
    
    5064703.344199/Main.c: In function ‘main’:
    5064703.344199/Main.c:8:49: error: expected expression before ‘%’ token

---
+++ 1.1.06.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+char input_1;
+int input_2;
+float input_3;
+double input_4;
+scanf("%s %d %f %lf",&input_1,&input_2,&input_3,%input_4);
+printf("%s %d %.6f %.6lf",input_1,input_2,input_3,input_4);
+}
